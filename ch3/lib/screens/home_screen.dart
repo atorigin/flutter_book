abf256835543c68aa295540deb864adaa5f24b43
atorigin <qaz60908@gmail.com>
2022-03-10 23:34:22 +0800

    練習 3-5-2

--- ch3/lib/screens/home_screen.dart
+++ ch3/lib/screens/home_screen.dart
@@ -7,12 +7,20 @@ class HomeScreen extends StatelessWidget {
     // ignore: prefer_const_constructors
     return Scaffold(
       body: Container(
+        alignment: Alignment.topCenter,
+        width: 500,
+        height: 500,
         margin: EdgeInsets.all(20),
         color: Colors.blue,
         child: Container(
+          alignment: Alignment.bottomRight,
+          width: 200,
+          height: 200,
           margin: EdgeInsets.all(20),
           color: Colors.red,
           child: Container(
+            width: 100,
+            height: 100,
             margin: EdgeInsets.all(20),
             color: Colors.black,
           ),
