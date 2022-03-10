15869627bf789f6b9c64f3b7b71594b701c79444
atorigin <qaz60908@gmail.com>
2022-03-10 18:41:48 +0800

    練習 3-4-2 並且新增造成 error 的程式碼在註解區

--- ch3/lib/screens/home_screen.dart
+++ ch3/lib/screens/home_screen.dart
@@ -5,11 +5,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
+    // 錯誤版本的 , ch3-4-2 拿掉 Expanded
+    // return Scaffold(
+    //   body: Column(
+    //     children: [
+    //       Container(
+    //         width: 500,
+    //         height: 500,
+    //         color: Colors.blue,
+    //         child: Text(
+    //           "大頭照預留區",
+    //           style: TextStyle(fontSize: 30),
+    //         ),
+    //       ),
+    //       Container(
+    //         width: 200,
+    //         height: 200,
+    //         color: Colors.red,
+    //         child: Text(
+    //           "簡介",
+    //           style: TextStyle(fontSize: 30),
+    //         ),
+    //       ),
+    //       Container(
+    //         color: Colors.amber,
+    //         child: Text(
+    //           "座右銘",
+    //           style: TextStyle(fontSize: 30),
+    //         )
+    //       )
+    //     ]
+    //   ),
+    // );
     return Scaffold(
       body: Column(
         children: [
           Expanded(
             child: Container(
+              width: 500,
+              height: 500,
               color: Colors.blue,
               child: Text(
                 "大頭照預留區",
@@ -19,6 +53,8 @@ class HomeScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              height: 200,
+              width: 200,
               color: Colors.red,
               child: Text(
                 "簡介",
