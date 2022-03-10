8ab0098cd32627d959e2d36edd33945a0a8dfa96
atorigin <qaz60908@gmail.com>
2022-03-10 18:25:03 +0800

    練習 3-4-2

--- ch3/lib/screens/home_screen.dart
+++ ch3/lib/screens/home_screen.dart
@@ -8,27 +8,33 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            color: Colors.blue,
-            child: Text(
-              "大頭照預留區",
-              style: TextStyle(fontSize: 30),
-            ),
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+              child: Text(
+                "大頭照預留區",
+                style: TextStyle(fontSize: 30),
+              ),
+            )
           ),
-          Container(
-            color: Colors.red,
-            child: Text(
-              "簡介",
-              style: TextStyle(fontSize: 30),
-            ),
+          Expanded(
+            child: Container(
+              color: Colors.red,
+              child: Text(
+                "簡介",
+                style: TextStyle(fontSize: 30),
+              ),
+            )
           ),
-          Container(
-            color: Colors.amber,
-            child: Text(
-              "座右銘",
-              style: TextStyle(fontSize: 30),
-            ),
-          )
+          Expanded(
+            child: Container(
+              color: Colors.amber,
+              child: Text(
+                "座右銘",
+                style: TextStyle(fontSize: 30),
+              ),
+            )
+          ),
         ],
       )
       );
