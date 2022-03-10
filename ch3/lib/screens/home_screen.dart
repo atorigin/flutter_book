70b138ce9b5e03941a226039da143bccddfa898b
atorigin <qaz60908@gmail.com>
2022-03-10 22:25:40 +0800

    練習 3-5-1

--- ch3/lib/screens/home_screen.dart
+++ ch3/lib/screens/home_screen.dart
@@ -5,110 +5,18 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
-    // 錯誤版本的 , ch3-4-2 拿掉 Expanded
-    // return Scaffold(
-    //   body: Column(
-    //     children: [
-    //       Container(
-    //         width: 500,
-    //         height: 500,
-    //         color: Colors.blue,
-    //         child: Text(
-    //           "大頭照預留區",
-    //           style: TextStyle(fontSize: 30),
-    //         ),
-    //       ),
-    //       Container(
-    //         width: 200,
-    //         height: 200,
-    //         color: Colors.red,
-    //         child: Text(
-    //           "簡介",
-    //           style: TextStyle(fontSize: 30),
-    //         ),
-    //       ),
-    //       Container(
-    //         color: Colors.amber,
-    //         child: Text(
-    //           "座右銘",
-    //           style: TextStyle(fontSize: 30),
-    //         )
-    //       )
-    //     ]
-    //   ),
-    // );
-
-    // 3-4-2 Expanded
-    // return Scaffold(
-    //   body: Column(
-    //     children: [
-    //       Expanded(
-    //         child: Container(
-    //           width: 500,
-    //           height: 500,
-    //           color: Colors.blue,
-    //           child: Text(
-    //             "大頭照預留區",
-    //             style: TextStyle(fontSize: 30),
-    //           ),
-    //         )
-    //       ),
-    //       Expanded(
-    //         child: Container(
-    //           height: 200,
-    //           width: 200,
-    //           color: Colors.red,
-    //           child: Text(
-    //             "簡介",
-    //             style: TextStyle(fontSize: 30),
-    //           ),
-    //         )
-    //       ),
-    //       Expanded(
-    //         child: Container(
-    //           color: Colors.amber,
-    //           child: Text(
-    //             "座右銘",
-    //             style: TextStyle(fontSize: 30),
-    //           ),
-    //         )
-    //       ),
-    //     ],
-    //   )
-    //   );
-
-    // 3-4-3 SingleChildScrollView
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: 500,
-              height: 500,
-              color: Colors.blue,
-              child: Text(
-                "大頭照預留區",
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            Container(
-              width: 200,
-              height: 200,
-              color: Colors.red,
-              child: Text(
-                "簡介",
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            Container(
-              color: Colors.amber,
-              child: Text(
-                "座右銘",
-                style: TextStyle(fontSize: 30),
-              ),
-            )
-          ]
-        ), 
+      body: Container(
+        margin: EdgeInsets.all(20),
+        color: Colors.blue,
+        child: Container(
+          margin: EdgeInsets.all(20),
+          color: Colors.red,
+          child: Container(
+            margin: EdgeInsets.all(20),
+            color: Colors.black,
+          ),
+        ),
       ),
     );
   }
