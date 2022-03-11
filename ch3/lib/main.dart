1f84053cf00d1a93e42012ca291ce5732a4e1eea
atorigin <qaz60908@gmail.com>
2022-03-11 19:29:02 +0800

    練習 3-6

--- ch3/lib/main.dart
+++ ch3/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:ch3/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:ch3/screens/second_screen.dart';
 
 class AppEntryPoint extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      routes: {
+        "/": (BuildContext context) => HomeScreen(),
+        "/second": (BuildContext context) => SecondScreen(),
+      },
+      initialRoute: "/",
     );
   }
 }
---
+++ ch3/lib/screens/second_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("第二個頁面"),
+    );
+  }
+}
